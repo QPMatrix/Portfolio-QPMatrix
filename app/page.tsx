39c50c90c345bbca1c39557a2e8e0f4325b50182
QPM <admin@QuantumPinnacleMatrix.tech>
2024-05-21 00:28:54 +0300

    done with portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import Approach from "@/components/approach";
 import Clients from "@/components/clients";
 import Experience from "@/components/experience";
+import Footer from "@/components/footer";
 import Grid from "@/components/grid";
 import Hero from "@/components/hero";
 import RecentProjects from "@/components/recent-projects";
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import { navItems } from "@/data";
-import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col  mx-auto sm:px-10">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
@@ -19,6 +19,7 @@ export default function Home() {
         <Clients />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
